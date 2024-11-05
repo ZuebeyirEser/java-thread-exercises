e301572a421ca8b72a002bc6e8b473b07b57a28a
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-05 10:41:24 +0100

    added countDownLatch method with 2 countdown example

--- src/headfirstjava/threads/PredictableLatch.java
+++ src/headfirstjava/threads/PredictableLatch.java
@@ -6,11 +6,20 @@ import java.util.concurrent.Executors;
 
 public class PredictableLatch {
     public static void main (String[] args) {
+        /*
+        *   The code is really similar to the code that performs a sleep; the main difference is the
+        *   latch.countDown in the main method. The performance difference is significant, though.
+        *   Instead of having to wait at least two seconds to make sure main has printed its message,
+        *   the new thread waits only until the main method has printed its “back in main” message.*/
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        CountDownLatch latch = new CountDownLatch(1);
+        CountDownLatch latch = new CountDownLatch(2);
         executor.execute(() -> waitForLatchThenPrint(latch));
         System.out.println("back in main");
 
+        latch.countDown();
+        System.out.println("some");
+        executor.execute(() -> waitForLatchThenPrint(latch));
+        System.out.println("some");
         latch.countDown();
         executor.shutdown();
     }
