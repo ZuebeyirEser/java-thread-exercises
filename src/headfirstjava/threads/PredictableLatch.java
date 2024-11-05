924d92c4293cc4ae895582abcb0c65d8f000460e
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-05 10:37:34 +0100

    added countDownLatch method

---
+++ src/headfirstjava/threads/PredictableLatch.java
@@ -0,0 +1,25 @@
+package headfirstjava.threads;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class PredictableLatch {
+    public static void main (String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        CountDownLatch latch = new CountDownLatch(1);
+        executor.execute(() -> waitForLatchThenPrint(latch));
+        System.out.println("back in main");
+
+        latch.countDown();
+        executor.shutdown();
+    }
+    private static void waitForLatchThenPrint(CountDownLatch latch) {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("top o’ the stack");
+    }
+}
