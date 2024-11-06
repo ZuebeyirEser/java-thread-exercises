b921a84aba7779c9df1633915ac39bf9dbb1357c
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-11-06 21:05:56 +0100

    added instuments name and instruments fields

--- src/headfirstjava/beatbox/BeatBoxFinal.java
+++ src/headfirstjava/beatbox/BeatBoxFinal.java
@@ -23,5 +23,12 @@ public class BeatBoxFinal {
     private Sequencer sequencer;
     private Sequence sequence;
     private Track track;
+    String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat",
+            "Open Hi-Hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap",
+            "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga",
+            "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo",
+            "Open Hi Conga"};
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+
 
 }
